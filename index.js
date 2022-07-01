4f94f35d3ccc969bd9d4bbe9447adbfe03bd3c3e
nosianuri <nurinosia@gmail.com>
2022-07-02 01:22:21 +0600

    update

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 const express = require('express');
+const cors = require('express');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+ 
 
-app.get('/', (req, res) =>{
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dw7pqr9.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const taskCollection = client.db("web-app").collection("task");
+    
+        app.get('/task', async(req, res) =>{
+            const query = {};
+            const cursor = taskCollection.find(query);
+            const tasks = await cursor.toArray();
+            res.send(tasks);
+        })
+    }
+
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
     res.send('Running my Web App Server');
 });
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log('Web App Server is running');
 })
